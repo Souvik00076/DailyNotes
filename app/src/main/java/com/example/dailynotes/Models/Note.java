106073d5e5d3a5a3671a4d0f6b0f3937b6f7ed81
Souvik00076 <souvikbhattacharjee00076@gmail.com>
2023-11-18 13:29:20 +0530

    MVVM Day 2

--- app/src/main/java/com/example/dailynotes/Models/Note.java
+++ app/src/main/java/com/example/dailynotes/Models/Note.java
@@ -42,87 +42,30 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.List;
 import java.util.concurrent.Executor;
 
-public class Note extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, RecognitionListener {
-    private EditText showTitle, showDescription;
+public class Note extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     private Uri productUri;
-    private String productID;
-    private LinearLayout layout;
     private static final int LOADER_NO = 2;
-    private FloatingActionButton changeColor;
-    private FloatingActionButton speech;
-    private String[] colorValues;
-    private String[] textColors;
-    private int pickUpValues;
-
-    private SpeechRecognizer recognizer;
-    private Intent recognizerIntent;
     public NoteModel noteModel;
-
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         ActivityNoteBinding noteBinding = DataBindingUtil
                 .setContentView(this, R.layout.activity_note);
-        noteModel = new NoteModel(getResources(), this);
+        noteModel = new NoteModel(getResources(), this, getContentResolver(), getIntent());
         noteBinding.setNotemodel(noteModel);
         noteBinding.setLifecycleOwner(this);
-        /*
-        setContentView(R.layout.activity_note);
-        noteModel=new NoteModel()
-
         if (null != getSupportActionBar()) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        initViews();
         productUri = getIntent().getData();
         if (productUri != null) {
-            productID = getIntent().getStringExtra("ID");
             LoaderManager.getInstance(this).initLoader(LOADER_NO, null, this);
         }
-
-        changeColor.setOnClickListener(view -> {
-            pickUpValues++;
-            if (pickUpValues == colorValues.length) pickUpValues = 0;
-            setColor(pickUpValues);
-        });
-        speech.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                convertSpeech();
-            }
-        });
-    */
-
     }
-
-    private void convertSpeech() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
-            onRecordAudioPermissionGranted();
-        } else {
-            int PERMISSIONS_REQUEST = 1;
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST);
-        }
-
-    }
-
-    private void onRecordAudioPermissionGranted() {
-        recognizer.startListening(recognizerIntent);
-    }
-
-    public void setColor(int pickUpValues) {
-        layout.setBackgroundColor(Color.parseColor(colorValues[pickUpValues]));
-        showTitle.setHintTextColor(Color.parseColor(textColors[pickUpValues]));
-        showTitle.setTextColor(Color.parseColor(textColors[pickUpValues]));
-        showDescription.setHintTextColor(Color.parseColor(textColors[pickUpValues]));
-        showDescription.setTextColor(Color.parseColor(textColors[pickUpValues]));
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.note_menu, menu);
         return true;
     }
-
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -132,84 +75,13 @@ public class Note extends AppCompatActivity implements LoaderManager.LoaderCallb
                 onBackPressed();
                 return true;
             case R.id.Save_Note:
-
-                /*if (getValidityChecked()) {
-                    Toast.makeText(this, "Title Missing", Toast.LENGTH_SHORT).show();
-                } else {
-                    insertOrUpdate();
-                    onBackPressed();
-                }*/
                 noteModel.onSaveClick();
+                onBackPressed();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
-
-    private void insertOrUpdate() {
-        ContentValues values = insertData();
-        int rows = 0;
-        Executor executor = AppExecutor.getInstance().getDiskIO();
-        if (null == productUri) {
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    Uri newUri = getContentResolver().insert(DailyNotesContracts.databaseEntry.CONTENT_URI, values);
-                }
-            });
-
-        } else {
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    values.put(DailyNotesContracts.databaseEntry.PAGE_ID, productID);
-                    getContentResolver().update(productUri, values, null, null);
-
-                }
-            });
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (recognizer != null) {
-            recognizer.destroy();
-            recognizer = null;
-        }
-    }
-
-    private ContentValues insertData() {
-        ContentValues values = new ContentValues();
-        String title = showTitle.getText().toString().trim();
-        String description = showDescription.getText().toString().trim();
-        long unixTime = System.currentTimeMillis() / 1000L;
-        values.put(DailyNotesContracts.databaseEntry.TITLE_COL, title);
-        values.put(DailyNotesContracts.databaseEntry.DESCRIPTION_COL, description);
-        values.put(DailyNotesContracts.databaseEntry.DATE_COL, unixTime);
-        values.put(DailyNotesContracts.databaseEntry.COLOR_INDEX, colorValues[pickUpValues]);
-        return values;
-    }
-
-    private boolean getValidityChecked() {
-        return showTitle.getText().toString().isEmpty();
-    }
-
-    private void initViews() {
-        pickUpValues = 0;
-        layout = findViewById(R.id.note);
-        showDescription = layout.findViewById(R.id.descp);
-        showTitle = layout.findViewById(R.id.title);
-        changeColor = findViewById(R.id.changeColor);
-        colorValues = getResources().getStringArray(R.array.color_values);
-        textColors = getResources().getStringArray(R.array.text_colors);
-        speech = (FloatingActionButton) findViewById(R.id.speech);
-        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        recognizer = SpeechRecognizer.createSpeechRecognizer(this);
-        recognizer.setRecognitionListener(this);
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-    }
-
     @NonNull
     @Override
     public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
@@ -223,75 +95,19 @@ public class Note extends AppCompatActivity implements LoaderManager.LoaderCallb
         return new CursorLoader(getApplicationContext(), productUri, projection,
                 null, null, null);
     }
-
     @Override
     public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
         data.moveToFirst();
         int titleColIndex = data.getColumnIndex(DailyNotesContracts.databaseEntry.TITLE_COL);
         int descpColIndex = data.getColumnIndex(DailyNotesContracts.databaseEntry.DESCRIPTION_COL);
         int colorIndex = data.getColumnIndex(DailyNotesContracts.databaseEntry.COLOR_INDEX);
-        showDescription.setText(data.getString(descpColIndex));
-        showTitle.setText(data.getString(titleColIndex));
         String color = data.getString(colorIndex);
-        for (int i = 0; i < colorValues.length; i++)
-            if (colorValues[i].equals(color))
-                pickUpValues = i;
-        //setColor(pickUpValues);
-
+        final String title = data.getString(titleColIndex);
+        final String description = data.getString(descpColIndex);
+        noteModel.setData(title, description, color);
         LoaderManager.getInstance(this).destroyLoader(LOADER_NO);
     }
-
     @Override
     public void onLoaderReset(@NonNull Loader<Cursor> loader) {
-
-    }
-
-    @Override
-    public void onReadyForSpeech(Bundle bundle) {
-
-    }
-
-    @Override
-    public void onBeginningOfSpeech() {
-
-    }
-
-    @Override
-    public void onRmsChanged(float v) {
-
-    }
-
-    @Override
-    public void onBufferReceived(byte[] bytes) {
-
-    }
-
-    @Override
-    public void onEndOfSpeech() {
-        recognizer.stopListening();
-    }
-
-    @Override
-    public void onError(int i) {
-
-    }
-
-    @Override
-    public void onResults(Bundle bundle) {
-
-        List<String> result = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-        for (String partial : result) {
-            showDescription.append(partial + " ");
-        }
-    }
-
-    @Override
-    public void onPartialResults(Bundle bundle) {
-
-    }
-
-    @Override
-    public void onEvent(int i, Bundle bundle) {
-
     }
 }
--- app/src/main/java/com/example/dailynotes/viewmodels/NoteModel.java
+++ app/src/main/java/com/example/dailynotes/viewmodels/NoteModel.java
@@ -1,10 +1,10 @@
 package com.example.dailynotes.viewmodels;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.Color;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
@@ -14,8 +14,12 @@ import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
 import androidx.databinding.ObservableInt;
 
+import com.example.dailynotes.Adapters.AppExecutor;
 import com.example.dailynotes.BR;
+import com.example.dailynotes.Data.DailyNotesContracts;
 import com.example.dailynotes.R;
+
+import java.util.concurrent.Executor;
 
 public class NoteModel extends BaseObservable {
     private ContentValues contentValues;
@@ -24,48 +28,67 @@ public class NoteModel extends BaseObservable {
     private Context _this;
     private SpeechRecognizer recognizer;
     private Intent recognizerIntent;
-    public final ObservableInt backgroundObservable = new ObservableInt();
+    private final ObservableInt backgroundObservable = new ObservableInt();
+    private Intent intent;
+    private ContentResolver resolver;
 
-    public NoteModel(Resources resources, Context context) {
+    public NoteModel(Resources resources, Context context, ContentResolver resolver, Intent intent) {
         contentValues = new ContentValues();
+        this.intent = intent;
+        this.resolver = resolver;
         _initConfig(resources, context);
+
+
     }
+
     @Bindable
     public int getBackgroundObservable() {
         return backgroundObservable.get();
     }
-    public void setBackgroundObservable(int val){
+
+    public void setBackgroundObservable(int val) {
         backgroundObservable.set(val);
         notifyPropertyChanged(BR.backgroundObservable);
     }
+
     @Bindable
     public String getTitle() {
-        return contentValues.getAsString("title");
+        return contentValues.getAsString(DailyNotesContracts.databaseEntry.TITLE_COL);
     }
 
     @Bindable
     public String getDescription() {
-        return contentValues.getAsString("description");
+        return contentValues.getAsString(DailyNotesContracts.databaseEntry.DESCRIPTION_COL);
     }
 
     public void setTitle(String title) {
-        contentValues.put("title", title);
+        System.out.println("title working");
+        contentValues.put(DailyNotesContracts.databaseEntry.TITLE_COL, title);
         notifyPropertyChanged(BR.title);
     }
 
     public void setDescription(String description) {
-        contentValues.put("description", description);
+        System.out.println("description working");
+        contentValues.put(DailyNotesContracts.databaseEntry.DESCRIPTION_COL, description);
         notifyPropertyChanged(BR.description);
     }
 
     public void setColor() {
-        Log.i("Here","Color Changed");
+        Log.i("Here", "Color Changed");
         setBackgroundObservable(pickUpValues++);
         pickUpValues %= colorValues.length;
     }
 
     public void onSaveClick() {
-        System.out.println("On saved called");
+        insertData();
+    }
+
+    private void insertData() {
+        long unixTime = System.currentTimeMillis() / 1000L;
+        contentValues.put(DailyNotesContracts.databaseEntry.DATE_COL, unixTime);
+        contentValues.put(DailyNotesContracts.databaseEntry.COLOR_INDEX, colorValues[pickUpValues]);
+        System.out.println("InsertData");
+        insertOrUpdate();
     }
 
     private void _initConfig(Resources resources, Context context) {
@@ -77,6 +100,31 @@ public class NoteModel extends BaseObservable {
         recognizer = SpeechRecognizer.createSpeechRecognizer(_this);
         recognizer.setRecognitionListener((RecognitionListener) _this);
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+    }
 
+    public void setData(String title, String description, String color) {
+        for (int i = 0; i < colorValues.length; i++)
+            if (colorValues[i].equals(color))
+                pickUpValues = i;
+        setTitle(title);
+        setDescription(description);
+    }
+
+    private void insertOrUpdate() {
+        System.out.println(contentValues.get(DailyNotesContracts.databaseEntry.DESCRIPTION_COL));
+        System.out.println(contentValues.get(DailyNotesContracts.databaseEntry.TITLE_COL));
+        System.out.println("InsertOrUpdate");
+
+        Executor executor = AppExecutor.getInstance().getDiskIO();
+        if (null == intent.getData()) {
+            executor.execute(() -> resolver.insert(DailyNotesContracts.databaseEntry.CONTENT_URI, contentValues));
+
+        } else {
+            String productID = intent.getStringExtra("ID");
+            executor.execute(() -> {
+                contentValues.put(DailyNotesContracts.databaseEntry.PAGE_ID, productID);
+                resolver.update(intent.getData(), contentValues, null, null);
+            });
+        }
     }
 }
